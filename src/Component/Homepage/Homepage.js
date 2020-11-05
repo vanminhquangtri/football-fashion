48e7df3f022f5f818bcb177977fd73520461b80c
Van Minh <vanminhquangtri@gmail.com>
2020-11-05 10:34:19 +0700

    done logos

--- src/Component/Homepage/Homepage.js
+++ src/Component/Homepage/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import MostConcerned from './MostConcerned/MostConcerned';
 import TopSlide from './TopSlide/TopSlide';
 
 const Homepage = () => {
     return (
         <>
             <TopSlide></TopSlide>
+            <MostConcerned/>
         </>
     );
 };
---
+++ src/Component/Homepage/MostConcerned/MostConcerned.js
@@ -0,0 +1,18 @@
+// direct child of Homepage
+import React from 'react';
+
+const MostConcerned = () => {
+    return (
+        <section className="most-concerned">
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col">
+                        Most Concerned
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default MostConcerned;
