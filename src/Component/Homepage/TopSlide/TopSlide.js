638d17f12f2bf0576d52e8b766f0bf89da205670
Van Minh <vanminhquangtri@gmail.com>
2020-11-06 10:17:00 +0700

    done top slide

--- src/Component/Homepage/TopSlide/TopSlide.js
+++ src/Component/Homepage/TopSlide/TopSlide.js
@@ -23,25 +23,25 @@ const TopSlide = () => {
                         <div className="wrap">
                             <div id="top-slide" className="carousel slide" data-ride="carousel">
                                 <div className="carousel-inner">
-                                    <div className="carousel-item active" data-interval="10000000">
-                                        <img src={require("../../../Assets/images/section-top-slide/tot.jpg").default} className="d-block w-100" alt="..." />
-                                        <div className="carousel-caption d-none d-md-block">
-                                            <h5>Sản phẩm mới nhất</h5>
-                                            <a href="/">Truy cập ngay</a>
-                                        </div>
-                                    </div>
-                                    <div className="carousel-item" data-interval="10000000">
-                                        <img src={require("../../../Assets/images/section-top-slide/ars.jpg").default} className="d-block w-100" alt="..." />
-                                        <div className="carousel-caption d-none d-md-block">
-                                            <h5>Kênh phân phối chính thức</h5>
-                                            <a href="/">Truy cập ngay</a>
-                                        </div>
-                                    </div>
-                                    <div className="carousel-item" data-interval="10000000">
+                                    <div className="carousel-item active mc" data-interval="4000">
                                         <img src={require("../../../Assets/images/section-top-slide/mc.jpg").default} className="d-block w-100" alt="..." />
-                                        <div className="carousel-caption d-none d-md-block">
-                                            <h5>Top 5 giải đấu hàng đầu</h5>
-                                            <a href="/">Truy cập ngay</a>
+                                        <div className="carousel-caption">
+                                            <span className="title">Sản phẩm mới nhất</span>
+                                            <NavLink to="/top-sales" className="link">Truy cập ngay</NavLink>
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item ars" data-interval="4000">
+                                        <img src={require("../../../Assets/images/section-top-slide/ars.jpg").default} className="d-block w-100" alt="..." />
+                                        <div className="carousel-caption">
+                                            <span className="title">Kênh phân phối chính thức</span>
+                                            <NavLink to="/top-viewed" className="link">Truy cập ngay</NavLink>
+                                        </div>
+                                    </div>
+                                    <div className="carousel-item tot" data-interval="4000">
+                                        <img src={require("../../../Assets/images/section-top-slide/tot.jpg").default} className="d-block w-100" alt="..." />
+                                        <div className="carousel-caption">
+                                            <span className="title">Top 5 giải đấu hàng đầu</span>
+                                            <NavLink to="/champion" className="link">Truy cập ngay</NavLink>
                                         </div>
                                     </div>
                                 </div>
