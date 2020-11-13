68f92fe8842c1b94703e4817f38e6cf5137ca7fe
Van Minh <vanminhquangtri@gmail.com>
2020-11-13 17:38:52 +0700

    toogle product summary

--- src/Component/Checkout/Checkout.js
+++ src/Component/Checkout/Checkout.js
@@ -1,11 +1,42 @@
 // direct child of Router URL
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {connect} from "react-redux";
+import ProductsInfo from '../../Data/ProductInfo';
 import CheckoutProduct from './CheckoutProduct/CheckoutProduct';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faChevronDown, faChevronUp} from "@fortawesome/free-solid-svg-icons"
 const Checkout = (props) => {
-    const {Cart, Currency, PaymentTarget} = props.Store;
-    console.log(PaymentTarget.target);
-    const PaidTarget = PaymentTarget.target
+    const {PaymentTarget} = props.Store;
+    const PaidTarget = PaymentTarget.target;
+    const [state, setState] = useState({
+        show_product_summary: false
+    })
+    // change status show_product_summary
+    const changeShow_product_summary = (ev) => {
+        ev.preventDefault();
+        setState((prevState) => {
+            return {
+                ...prevState,
+                show_product_summary: !prevState.show_product_summary
+            }
+        })
+    }
+    // calculate total amount of shopping cart
+    var totalAmount = 0;
+    // loop each product in cart
+    PaidTarget.forEach((product) => {
+        // get price of product
+        var productPrice;
+        ProductsInfo.forEach((p) => {
+            if (p.id === product.product_id) {
+                productPrice = p.price;
+            }
+        });
+        // loop all quantity object of product, multiply product price with quantity and plus to totalAmount
+        product.quantity.forEach((size_quantity) => {
+            totalAmount += productPrice * size_quantity.quantity
+        })
+    });
     useEffect(() => {
        window.scrollTo(0, 0); 
     },[])
@@ -19,20 +50,41 @@ const Checkout = (props) => {
                         </div>
                     </div>
                 </div>
-                <div className="row check-out-product-summary">
+                <div className="row toggle-product-summary">
                     <div className="col">
                         <div className="content">
-                            <span className="title">Đơn hàng của bạn</span>
-                            {
-                                PaidTarget.map((product) => {
-                                    return (
-                                        <CheckoutProduct product={product} key={product.id}/>
-                                    )
-                                })
+                            {state.show_product_summary === false ? (
+                                <div className="open-toggle" onClick={(ev)=>{changeShow_product_summary(ev)}}>
+                                    Hiển thị thông tin đơn hàng
+                                    <FontAwesomeIcon icon={faChevronDown} className="icon"/>
+                                    <span className="title">{totalAmount}</span>
+                                </div>
+                                ) : (
+                                <div className="close-toggle" onClick={(ev)=>{changeShow_product_summary(ev)}}>
+                                    Ẩn thông tin đơn hàng
+                                    <FontAwesomeIcon icon={faChevronUp} className="icon"/>
+                                </div>
+                                )
                             }
                         </div>
                     </div>
                 </div>
+                {state.show_product_summary && (
+                    <div className="row check-out-product-summary">
+                        <div className="col">
+                            <div className="content">
+                                {
+                                    PaidTarget.map((product) => {
+                                        return (
+                                            <CheckoutProduct product={product} key={product.id}/>
+                                        )
+                                    })
+                                }
+                                <span className="title">Tổng cộng: {totalAmount}</span>
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
         </section>
     );
