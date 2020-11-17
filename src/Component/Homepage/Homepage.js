3992924715c24bc59c7538dd5f02cda6e1ed8877
Van Minh <vanminhquangtri@gmail.com>
2020-11-17 12:17:29 +0700

    done top news

--- src/Component/Homepage/Homepage.js
+++ src/Component/Homepage/Homepage.js
@@ -1,10 +1,12 @@
 import React,{useEffect} from 'react';
+import {NavLink} from "react-router-dom";
 import TopViewed from './TopViewed/TopViewed';
 import TopSlide from './TopSlide/TopSlide';
 import SpecialPromotion from './SpecialPromotion/SpecialPromotion';
 import TopSales from './TopSales/TopSales';
 import Champion from './Champion/Champion';
 import GoalVideo from './GoalVideo/GoalVideo';
+import NewsImage from "../../Assets/images/section-home-page/news3.gif";
 
 const Homepage = () => {
     useEffect(() => {
@@ -12,6 +14,20 @@ const Homepage = () => {
     })
     return (
         <section className="homepage">
+            <div className="top-news">
+                <div className="content">
+                    <div className="news">
+                        <NavLink to="/promotion" className="link">
+                            Khuyến mãi siêu hấp dẫn
+                            <img className="news-image" alt="promotion" src={NewsImage}/>
+                        </NavLink>
+                        <NavLink to="/leagues" className="link">
+                            Khám phá những bộ cánh siêu đẳng cấp
+                            <img className="news-image" alt="promotion" src={NewsImage}/>
+                        </NavLink>
+                    </div>
+                </div>
+            </div>
             <TopSlide/>
             <TopViewed/>
             <SpecialPromotion/>
